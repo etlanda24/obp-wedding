bacbd6c7e3ac97dbaae2dc2e3e27f6e1c13de5bf
Yolanda Ihsan <yolanda.ihsan@8investment.com>
2021-01-27 21:46:57 +0700

    Test commit

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
   <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container"><a href="#"><svg class="heart" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
 </svg></a>
-      <h1 class="h2"><a class="pl-4 navbar-brand" href="#">Wonderful Wedding</a></h1>
+      <h1 class="h2"><a class="pl-4 navbar-brand" href="#">Not Wonderful Wedding</a></h1>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ww-navbarNav" aria-controls="ww-navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse text-uppercase" id="ww-navbarNav">
         <ul class="navbar-nav ml-auto">
